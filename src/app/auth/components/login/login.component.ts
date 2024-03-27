1bbd8b870c733b5d3c52068069589edb9636bb49
zeusopera9 <zaidalimerchant@yahoo.com>
2024-03-28 03:08:06 +0530

    Storage service and Role based navigation implemented

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
+import { StorageService } from '../../services/storage/storage.service';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +16,9 @@ export class LoginComponent {
   loginForm!:FormGroup;
 
   constructor(private fb:FormBuilder,
-    private authService: AuthService) { }
+    private authService: AuthService,
+    private router:Router,
+    private message: NzMessageService) { }
 
   ngOnInit(){
     this.loginForm = this.fb.group({
@@ -26,6 +31,21 @@ export class LoginComponent {
     console.log(this.loginForm.value);
     this.authService.login(this.loginForm.value).subscribe((res) => {
       console.log(res);
+      if(res.userId != null) {
+        const user = {
+          id: res.userId,
+          role: res.userRole,
+        }
+        StorageService.saveUser(user);
+        StorageService.saveToken(res.jwt);
+        if(StorageService.isAdminLoggedIn()) {
+          this.router.navigateByUrl("/admin/dashboard");
+        } else if(StorageService.isCustomerLoggedIn()) {
+          this.router.navigateByUrl("/customer/dashboard");
+        } else {
+          this.message.error("Bad Credentials",{nzDuration: 5000});
+        }
+      }
     })
   }
 }
