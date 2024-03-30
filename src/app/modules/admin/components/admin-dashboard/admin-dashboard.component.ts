f67c701661c840d0c2544855ceba5e5a6f46bcda
zeusopera9 <zaidalimerchant@yahoo.com>
2024-03-30 16:45:36 +0530

    Delete Car By ID Implemented

--- src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../services/admin.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -10,7 +12,9 @@ export class AdminDashboardComponent {
 
   cars: any = [];
 
-  constructor(private adminService: AdminService) {
+  constructor(private adminService: AdminService,
+    private message: NzMessageService, 
+    private router: Router) {
 
   }
 
@@ -25,6 +29,17 @@ export class AdminDashboardComponent {
         element.processedImg = 'data:image/jpeg;base64' + element.returnedImage;
         this.cars.push(element);
       })
+    })
+  }
+
+  deleteCar(id: number) {
+    console.log(id);
+    this.adminService.deleteCar(id).subscribe((res) => {
+      this.getAllCars();
+      this.message.success("Car Deleted Successfully", { nzDuration: 5000 });
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
     })
   }
 
--- src/app/modules/admin/services/admin.service.ts
+++ src/app/modules/admin/services/admin.service.ts
@@ -24,6 +24,12 @@ export class AdminService {
     })
   }
 
+  deleteCar(id:number): Observable<any> {
+    return this.http.delete(BASIC_URL + "/api/admin/car/" + id, {
+      headers: this.createAuthorizationHeader()
+    });
+  }
+
   createAuthorizationHeader(): HttpHeaders {
     let authHeaders: HttpHeaders = new HttpHeaders();
     return authHeaders.set(
